aaf14dce5c986cc526697ca7a693ad07d9e368ac
Tramaine Jackson <jackson.tramaine3@gmail.com>
2018-06-28 17:23:12 -0400

    General updates

--- resources/views/news/index.blade.php
+++ resources/views/news/index.blade.php
@@ -20,44 +20,50 @@
 	<section class="extra-margins pb-3 wow fadeIn" data-wow-delay="0.3s">
 
 		<!--Section heading-->
-		<h3 class="text-center font-weight-bold h3 pb-5 pt-3">Recent posts</h3>
+		<h3 class="text-center font-weight-bold h3 pb-5 pt-3 white-text">Recent posts</h3>
 
 		<!--Grid row-->
 		<div class="row">
-			@foreach($articles as $article)
-				<!--Grid column-->
-				<div class="col-lg-4 col-md-12 mb-4">
+			@if($articles->count() > 0)
+				@foreach($articles as $article)
+					<!--Grid column-->
+					<div class="col-lg-4 col-md-12 mb-4">
 
-					<!--Card Light-->
-					<div class="card">
-						<!--Card image-->
-						<div class="view overlay">
-							<img  class="card-img-top" src="{{ $article->picture !== null ? asset(str_ireplace('public/images', 'storage/images/lg', $article->picture)) : $defaultImg }}" alt="">
-							<a>
-								<div class="mask rgba-white-slight"></div>
-							</a>
+						<!--Card Light-->
+						<div class="card">
+							<!--Card image-->
+							<div class="view overlay">
+								<img  class="card-img-top" src="{{ $article->picture !== null ? asset(str_ireplace('public/images', 'storage/images/lg', $article->picture)) : $defaultImg }}" alt="">
+								<a>
+									<div class="mask rgba-white-slight"></div>
+								</a>
+							</div>
+							<!--/.Card image-->
+							<!--Card content-->
+							<div class="card-body">
+								<!--Social shares button-->
+								<a class="activator p-3 mr-2"><i class="fa fa-share-alt"></i></a>
+								<!--Title-->
+								<h4 class="card-title">{{ $article->title }}</h4>
+								<hr>
+								<!--Text-->
+								<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+								<a href="{{ route('news.show', ['news' => $article->id]) }}" class="link-text"><h5>Read more <i class="fa fa-chevron-right"></i></h5></a>
+							</div>
+							<!--/.Card content-->
 						</div>
-						<!--/.Card image-->
-						<!--Card content-->
-						<div class="card-body">
-							<!--Social shares button-->
-							<a class="activator p-3 mr-2"><i class="fa fa-share-alt"></i></a>
-							<!--Title-->
-							<h4 class="card-title">{{ $article->title }}</h4>
-							<hr>
-							<!--Text-->
-							<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-							<a href="{{ route('news.show', ['news' => $article->id]) }}" class="link-text"><h5>Read more <i class="fa fa-chevron-right"></i></h5></a>
-						</div>
-						<!--/.Card content-->
+						<!--/.Card Light-->
+
 					</div>
-					<!--/.Card Light-->
-
+					<!--Grid column-->
+				
+				@endforeach
+			@else
+				<div class="col text-center my-4">
+					<h1 class="h1-responsive">There aren't any post at this time.</h1>
+					<h3 class="h3-responsive">We are currently looking for journalist who love the game of basketball and who follows the sport for all ages to write articles. If this sounds like something you're interest in, please reach out to us.</h3>
 				</div>
-				<!--Grid column-->
-			
-			@endforeach
-			
+			@endif
 		</div>
 		<!--Grid row-->
 
